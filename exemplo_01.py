b44a331d8758241d68f8a374c0b613dd8240beca
jemima1992 <jemimafpassos@gmail.com>
2024-06-22 14:01:13 -0300

    adicionando o exercicio 1 e 2

---
+++ exemplo_01.py
@@ -0,0 +1,5 @@
+# Exercício 01
+
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(len(input("Digite o seu nome: ")))
---
+++ exemplo_02.py
@@ -0,0 +1,12 @@
+# Exercício 02
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite o primeiro numero")) + int(input("Digite o segundo numero")))
+
+
+numero = 3 # int
+print(type(numero))
+numero_decimal = 4.78 # float 
+nome_de_usuario = "Liciano" #string
+verdadeiro = True # bool
